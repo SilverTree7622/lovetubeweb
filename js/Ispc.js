45c689737e950f16776712bd29dfc158221cef5b
YDH SilverFrog <dong5966@naver.com>
2018-12-15 20:45:39 +0900

    Update Ispc.js

--- js/Ispc.js
+++ js/Ispc.js
@@ -23,10 +23,10 @@ function widthdetect() {
     var windowWidth = $( window ).width();
   if(windowWidth <= 770) {
   //창 가로 크기가 770 이상일 경우
-  console.log("not pc로 인식");
+  console.log("가로 화면 px크기에 따라서 not pc로 인식");
   } else {
   //창 가로 크기가 770 보다 클 경우
-  console.log("pc로 인식");
+  console.log("가로 화면 px크기에 따라서 pc로 인식");
   }
 
   console.log("보이는 창 크기 : " + windowWidth + "px");
